cc71e35c47d71c211c7f5b2b951d1827ae665980
Enfu Zhang <fanff0324@gmail.com>
2016-08-29 22:37:24 -0600

    Added the algorithm to let Harvesters to find more types of storages.
    Added the algorithm to let Harvesters to find more types of storages.
    Now testing on the transfering of harvesters to containers.

--- src/role.harvester.js
+++ src/role.harvester.js
@@ -12,17 +12,31 @@ var roleHarvester = {
             var targets = creep.room.find(FIND_STRUCTURES, {
                     filter: (structure) => {
                         return (structure.structureType == STRUCTURE_EXTENSION ||
+                                structure.structureType == STRUCTURE_TOWER ||
                                 structure.structureType == STRUCTURE_SPAWN ||
-                                structure.structureType == STRUCTURE_TOWER) && structure.energy < structure.energyCapacity;
+                                structure.structureType == STRUCTURE_STORAGE) && structure.energy < structure.energyCapacity;
                     }
             });
-            if(targets.length > 0) {
+            var findContainers = creep.room.find(FIND_STRUCTURES, {
+              filter: { structureType: STRUCTURE_CONTAINER }
+            });
+            var extensionResources = findContainers.energy < findContainers.energyCapacity;
+            if(extensionResources == true) {
+              if(targets.length > 0) {
                 if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                     creep.moveTo(targets[0]);
                 }
+              }
+              if(findContainers.length) {
+                if(container.store[RESOURCE_ENERGY] < container.storeCapacity) {
+                  if(creep.transfer(findContainers[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                      creep.moveTo(findContainers[0]);
+                  }
+                }
+              }
             }
         }
-	}
+	    }
 };
 
 module.exports = roleHarvester;
